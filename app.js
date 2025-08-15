e81de69edb2901701557570f3d43220975ca5804
devVivs <viviannacosta18@gmail.com>
2025-08-15 17:55:41 -0300

    javascript

--- app.js
+++ app.js
@@ -1,1 +1,40 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let amigos = [];
+
+function adicionarAmigo(){
+    let nomes = document.getElementById('amigo').value;
+
+    if (nomes == ""){
+        alert("Por favor, insira um nome.");
+    } else {
+        amigos.push(nomes);
+        limparCampo();
+        exibirLista();
+        return nomes;
+    }    
+}
+
+function limparCampo(){
+   nomes = document.querySelector('input');
+   nomes.value = "";
+   lista = document.getElementById('listaAmigos');
+   lista.innerHTML = "";
+}
+
+function exibirLista(){
+    let lista = document.getElementById('listaAmigos');
+    lista.innerHTML = "";
+     for (let i = 0; i < amigos.length; i++) {
+    lista.innerHTML += `<li>${amigos[i]}</li>`;
+  }
+}
+
+
+function sortearAmigo(){
+    let nomeAleatorio = Math.floor(Math.random() * amigos.length);
+    let resultado = document.getElementById('resultado');
+    limparCampo();
+    resultado.innerHTML = `<li>Seu amigo secreto é: ${amigos[nomeAleatorio]}</li>`;
+}
+
+
+
